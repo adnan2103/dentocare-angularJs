45018b67db9b00a289ed00cb1b79288626500b7f
Adnan Khan <adnan.khan@cdk.com>
2016-03-08 23:29:49 +0530

    added more code for treatment

--- src/main/resources/schemaDrop.sql
+++ src/main/resources/schemaDrop.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS  payment;
 DROP TABLE IF EXISTS  patient_oral_examination;
-DROP TABLE IF EXISTS  default_oral_examination;
 DROP TABLE IF EXISTS  treatment;
 DROP TABLE IF EXISTS  status;
 DROP TABLE IF EXISTS  doctor_patient_mapping;
--- src/main/webapp/resources/app/js/controllers/TreatmentController.js
+++ src/main/webapp/resources/app/js/controllers/TreatmentController.js
@@ -6,9 +6,34 @@
  */
 var TreatmentController = function($scope, $http, $routeParams) {
     $scope.fetchTreatment = function() {
+
+        $http.get('patient/' + $routeParams.id).success(function(patient){
+            $scope.patient = patient;
+        });
+
         $http.get('patient/' + $routeParams.id + '/treatment').success(function(treatments){
             $scope.treatments = treatments;
         });
+    };
+
+    $scope.saveTreatment = function(treatment) {
+        $scope.resetError();
+
+        $http.put('patient/' + $routeParams.id + '/treatment', treatment).success(function() {
+            $scope.message = 'Treatment Updated.';
+        }).error(function() {
+            $scope.setError('Could not update the treatment.');
+        });
+    };
+
+    $scope.resetError = function() {
+        $scope.error = false;
+        $scope.errorMessage = '';
+    };
+
+    $scope.setError = function(message) {
+        $scope.error = true;
+        $scope.errorMessage = message;
     };
 
     $scope.fetchTreatment();
