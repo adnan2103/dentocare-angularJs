dd4d325ba3cf931e1c2b36f9bfe5ae4ab5771807
Adnan Khan <adnan.khan@cdk.com>
2016-06-28 05:02:40 +0530

    Added Treatment Image Upload Complete Functionality

--- src/main/webapp/resources/app/js/controllers/TreatmentImageController.js
+++ src/main/webapp/resources/app/js/controllers/TreatmentImageController.js
@@ -4,7 +4,7 @@
  * TreatmentImageController
  * @constructor
  */
-var TreatmentImageController = function($scope, $http, $routeParams) {
+var TreatmentImageController = function($scope, $http, $routeParams, FileUploader) {
 
     $scope.fetchTreatmentImagePaths = function() {
 
@@ -18,4 +18,56 @@ var TreatmentImageController = function($scope, $http, $routeParams) {
 
     $scope.fetchTreatmentImagePaths();
 
+
+    var uploader = $scope.uploader = new FileUploader({
+        url: 'treatment/' + $routeParams.id + '/' + $routeParams.type + '/images/'
+    });
+
+    // FILTERS
+
+    uploader.filters.push({
+        name: 'imageFilter',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+    });
+
+    // CALLBACKS
+
+    uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+        console.info('onWhenAddingFileFailed', item, filter, options);
+    };
+    uploader.onAfterAddingFile = function(fileItem) {
+        console.info('onAfterAddingFile', fileItem);
+    };
+    uploader.onAfterAddingAll = function(addedFileItems) {
+        console.info('onAfterAddingAll', addedFileItems);
+    };
+    uploader.onBeforeUploadItem = function(item) {
+        console.info('onBeforeUploadItem', item);
+    };
+    uploader.onProgressItem = function(fileItem, progress) {
+        console.info('onProgressItem', fileItem, progress);
+    };
+    uploader.onProgressAll = function(progress) {
+        console.info('onProgressAll', progress);
+    };
+    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        console.info('onSuccessItem', fileItem, response, status, headers);
+    };
+    uploader.onErrorItem = function(fileItem, response, status, headers) {
+        console.info('onErrorItem', fileItem, response, status, headers);
+    };
+    uploader.onCancelItem = function(fileItem, response, status, headers) {
+        console.info('onCancelItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+        console.info('onCompleteItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteAll = function() {
+        console.info('onCompleteAll');
+    };
+
+    console.info('uploader', uploader);
 };
