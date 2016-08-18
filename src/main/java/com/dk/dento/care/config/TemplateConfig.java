145207a9eb48b515e7b566b0ebadc1497bb36d00
Adnan2103 <adnan2103@hotmail.com>
2016-08-18 14:30:58 +0530

    Some more changes

--- src/main/java/com/dk/dento/care/config/TemplateConfig.java
+++ src/main/java/com/dk/dento/care/config/TemplateConfig.java
@@ -13,7 +13,7 @@ public class TemplateConfig {
     @Bean
     public ServletContextTemplateResolver templateResolver() {
         ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-        resolver.setPrefix("/resources/app/view/");
+        resolver.setPrefix("/front-end/src/view/");
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setCacheable(false);
